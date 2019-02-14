640eddc6d3cac238dd867f211185c27e75fea0fa
Guangcan Chen <chenguangcan@live.com>
2019-02-15 01:02:44 +0800

    Initial commit

---
+++ SrtGoogleTranslate/SrtGoogleTranslate.py
@@ -0,0 +1,112 @@
+#!c:\python27\python.exe
+# -- coding: utf-8 --
+import io
+import os
+import sys
+import time
+import argparse
+import grequests
+from googletrans import Translator
+from googletrans.utils import format_json
+from retry import retry
+
+#"""Google翻译API"""
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+translator = Translator(service_urls=['translate.google.com'])
+
+#"""单句翻译函数"""
+@retry(delay=60)
+def sentenceTranslate(line):
+	line = line.strip()
+	try:
+		text = translator.translate(line,src='en',dest='zh-cn').text
+	except:
+		print 'Translate Failed. Please Change Your WAN IP Address!'
+	return text
+
+#"""术语库加载函数"""
+def loadReplaceWords(path):
+    replaceTableFile=io.open(path,mode='r',encoding='utf-8')
+    lines=[]
+    for line in replaceTableFile:
+        lines.append(line.replace('\n',''))
+    replaceTableFile.close()
+    dict={}
+    for lineIdx in range(len(lines)):
+        if lineIdx%3==0 and lines[lineIdx]!='':
+            dict.setdefault(lines[lineIdx],lines[lineIdx+1])
+    return dict
+
+#"""术语库批量替换函数"""
+def sentenceReplace(line,**dict):
+    for key in dict.keys():
+        line=line.replace(key,dict[key])
+    return line
+
+#"""单个SRT翻译函数"""
+def oneTranslate(path,**dict):
+    #"""字幕文件读取"""
+    tempFile = io.open(path,mode='r',encoding='utf-8')
+    lines=[]
+    for line in tempFile:
+        lines.append(line)
+    tempFile.close()
+
+    #"""不是空文件"""
+    if len(lines)>3:
+        #"""是否已翻译"""
+        isTranslate=False
+        if len(lines[4])==1:
+            isTranslate=True
+
+        #"""Google翻译"""
+        transLines=[]
+        if not isTranslate:
+            #"""并发翻译"""
+            #transLines=multiTranslate(lines)
+            #"""逐句翻译"""
+            for lineIdx in range(len(lines)):
+                if lineIdx%4==2:
+                    transLines.append(sentenceTranslate(lines[lineIdx])+'\n')
+                    #"""进度条"""
+                    sys.stdout.write(str(int((lineIdx*1.0)/len(lines)*10000)/100.0)+'%'+"\r")
+                    #"""防止谷歌BanIP"""
+                    time.sleep(0.3)
+                transLines.append(lines[lineIdx])
+
+            #"""列表转文本"""
+            totalContent=''.join(transLines)
+
+            #"""术语库替换"""
+            totalContent=sentenceReplace(totalContent,**dict)
+
+            #"""写入译文"""
+            tempFile = io.open(tempPath,mode='w+',encoding='utf-8')
+            tempFile.write(totalContent)
+            tempFile.close()
+            print(tempPath.split('\\')[-1])
+
+        #"""清理临时列表"""
+        del lines[:]
+        del transLines[:]
+
+#参数输入
+parser=argparse.ArgumentParser()
+parser.add_argument('-p',help='Path to the SRT Folder')
+parser.add_argument('-d',help='Path to the Dict File')
+args = parser.parse_args()
+if args.d:
+    #"""术语库路径"""
+    replaceTablePath=args.d
+    rDict=loadReplaceWords(replaceTablePath)
+else:
+    rDict={}
+if args.p:
+    #"""SRT文件夹路径"""
+    tempDirPath=args.p
+    #"""批量翻译SRT"""
+    for dirpath, dirnames, filenames in os.walk(tempDirPath):
+        for filepath in filenames:
+            tempPath = os.path.join(dirpath, filepath)
+            if filepath[-4:]=='.srt' or filepath[-4:]=='.SRT':
+                oneTranslate(tempPath,**rDict)
